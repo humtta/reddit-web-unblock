c4029f399c8408fbc27f6a1dbf8bff18c8be6432
Hugo Marotta <humtta@proton.me>
2026-07-10 17:29:43 -0300

    Add isPopup function
    isPopup checks whether a node is an element that matches a known popup
    tag.

--- src/inject.js
+++ src/inject.js
@@ -4,4 +4,11 @@
     "app-upsell-blocking-bottom-sheet-seo",
   ];
   const popupSelector = popupTags.join(",");
+
+  function isPopup(node) {
+    return (
+      node.nodeType === Node.ELEMENT_NODE &&
+      popupTags.includes(node.tagName.toLowerCase())
+    );
+  }
 })();
