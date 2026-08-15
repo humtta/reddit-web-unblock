f88204fa958c5b546a6bf26fa3d5f7d1a81e8fa8
Hugo Marotta <humtta@proton.me>
2026-08-15 12:23:29 -0300

    Add file paths in gen-script

--- scripts/gen-script.sh
+++ scripts/gen-script.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+
+# Project root directory
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+MANIFEST_FILE="${ROOT_DIR}/src/manifest.json"
+JS_FILE="${ROOT_DIR}/src/inject.js"
+
+# Paths relative to the project root directory
+ICON_FILE_REL="src/assets/icon.svg"
+SCRIPT_FILE_REL="script.user.js"
